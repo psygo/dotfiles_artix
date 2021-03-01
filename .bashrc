8dc257af590a3b0c4e774a07774177aec5d5d3c0
Philippe Fanaro <philippefanaro@gmail.com>
2021-03-01 07:35:12 -0300

    first setup of the dotfiles repo

---
+++ .bashrc
@@ -0,0 +1,7 @@
+export EDITOR=nvim
+
+alias vim="nvim"
+alias vi="nvim"
+alias oldvim="vim"
+alias ls="ls --color=auto"
+alias cat="bat"
---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,91 @@
+--------------------------------------------------------------------------------
+import XMonad
+import System.Exit
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+--------------------------------------------------------------------------------
+main = xmonad defaultConfig
+        { modMask = mod4Mask
+        , terminal = "konsole"
+        , borderWidth = 5
+	, normalBorderColor  = "#dddddd"
+	, focusedBorderColor = "ff0000"
+	, workspaces = ["1", "2", "3", "4", "5"]
+	, keys = myKeys
+        }
+--------------------------------------------------------------------------------
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [
+      ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm .|. shiftMask, xK_c     ), kill)
+    , ((modm              , xK_b     ), spawn "brave")
+    , ((modm,               xK_space ), sendMessage NextLayout)
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+    , ((modm,               xK_Return), windows W.swapMaster)
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+    ]
+    ++
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+--------------------------------------------------------------------------------
+help = unlines [
+    "The default modifier key is 'alt'. Default keybindings:",
+    "",
+    "-- Launching and killing programs",
+    "mod-Shift-Enter  Launch konsole",
+    "mod-p            Launch dmenu",
+    "mod-Shift-c      Close/kill the focused window",
+    "mod-Space        Rotate through the available layout algorithms",
+    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
+    "mod-n            Resize/refresh viewed windows to the correct size",
+    "",
+    "-- Move focus up or down the window stack",
+    "mod-Tab        Move focus to the next window",
+    "mod-Shift-Tab  Move focus to the previous window",
+    "mod-j          Move focus to the next window",
+    "mod-k          Move focus to the previous window",
+    "mod-m          Move focus to the master window",
+    "",
+    "-- Modifying the window order",
+    "mod-Return   Swap the focused window and the master window",
+    "mod-Shift-j  Swap the focused window with the next window",
+    "mod-Shift-k  Swap the focused window with the previous window",
+    "",
+    "-- Resizing the master/slave ratio",
+    "mod-h  Shrink the master area",
+    "mod-l  Expand the master area",
+    "",
+    "",
+    "-- Increase or decrease number of windows in the master area",
+    "mod-comma  (mod-,)   Increment the number of windows in the master area",
+    "mod-period (mod-.)   Deincrement the number of windows in the master area",
+    "",
+    "-- Quit, or restart",
+    "mod-Shift-q  Quit xmonad",
+    "mod-q        Restart xmonad",
+    "mod-[1..9]   Switch to workSpace N",
+    "",
+    "-- Workspaces & screens",
+    "mod-Shift-[1..9]   Move client to workspace N",
+    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
+    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
+    "",
+    "-- Mouse bindings: default actions bound to mouse events",
+    "mod-button1  Set the window to floating mode and move by dragging",
+    "mod-button2  Raise the window to the top of the stack",
+    "mod-button3  Set the window to floating mode and resize by dragging"
+    ]
+--------------------------------------------------------------------------------
