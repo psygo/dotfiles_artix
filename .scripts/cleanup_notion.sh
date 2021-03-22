d23a61dd94e911bf7edbce8bb5c4b403988bca7f
Philippe Fanaro <philippefanaro@gmail.com>
2021-03-22 19:53:48 -0300

    added the proper way of cleaning up Notion's hashcodes on the backup files and folders

--- .scripts/cleanup_notion.sh
+++ .scripts/cleanup_notion.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-find . -type f -name '*.csv' -delete
+# For more info, check my question on StackOverflow:
+# https://unix.stackexchange.com/q/640608/364698
+
+find . -depth -execdir perl-rename 's/ [a-z0-9]{32}//' {} +
+
+exit 0
