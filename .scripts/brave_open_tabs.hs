e8b3ed2b78bd0799a880356f3479f0f67ce9d1e5
Philippe Fanaro <philippefanaro@gmail.com>
2021-04-03 14:06:20 -0300

    better implementation of the brave_open_tabs script in Haskell

--- .scripts/brave_open_tabs.hs
+++ .scripts/brave_open_tabs.hs
@@ -1,3 +1,23 @@
+--------------------------------------------------------------------------------
+-- Use this to compile:
+--
+-- ```hs
+-- ghc --make -dynamic brave_open_tabs.hs -o bin/brave_open_tabs
+-- ```
+--
+-- And use this to run:
+--
+-- ```sh
+-- ./bin/brave_open_tabs <db-name>
+-- ```
+--------------------------------------------------------------------------------
+import System.Environment
 import System.Process
-
-main = callCommand "brave --new-window https://google.com"
+--------------------------------------------------------------------------------
+main = do
+          args        <- getArgs
+          content     <- readFile (head args)
+          let links    = lines     content
+          let commands = map      ("brave " ++) links
+          mapM callCommand commands
+--------------------------------------------------------------------------------
