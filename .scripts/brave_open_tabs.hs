3abbeda379b532bcbbd04b819a935caa260fd599
Philippe Fanaro <philippefanaro@gmail.com>
2021-04-03 14:48:08 -0300

    now the Haskell and bash script for opening tabs with Brave are in parity

--- .scripts/brave_open_tabs.hs
+++ .scripts/brave_open_tabs.hs
@@ -1,23 +1,35 @@
 --------------------------------------------------------------------------------
--- Use this to compile:
---
--- ```hs
--- ghc --make -dynamic brave_open_tabs.hs -o bin/brave_open_tabs
--- ```
---
--- And use this to run:
---
--- ```sh
--- ./bin/brave_open_tabs <db-name>
--- ```
+{-
+Use this to compile:
+
+```hs
+ghc --make -dynamic brave_open_tabs.hs -o ~/bin/brave_open_tabs
+```
+
+And use this to run:
+
+```sh
+./bin/brave_open_tabs ~/.scripts/db/<db-name>
+```
+-}
 --------------------------------------------------------------------------------
 import System.Environment
 import System.Process
 --------------------------------------------------------------------------------
 main = do
-          args        <- getArgs
-          content     <- readFile (head args)
-          let links    = lines     content
-          let commands = map      ("brave " ++) links
+          args         <- getArgs
+          content      <- readFile (head args)
+          let links     = lines    content
+          let numLinks  = length   links
+          let commands  = zipWith  (++) (braveCmdListGenerator numLinks) links
           mapM callCommand commands
+
+braveCmdListGenerator  :: Int -> [String]
+braveCmdListGenerator   = appendNewWindowToFirst . replicateBrave
+
+replicateBrave         :: Int -> [String]
+replicateBrave n        = replicate n "brave "
+
+appendNewWindowToFirst       :: [String] -> [String]
+appendNewWindowToFirst (x:xs) = (x ++ "--new-window "):xs
 --------------------------------------------------------------------------------
